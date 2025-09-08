f306b3b99a35f9817bd1d49569c92f1e6c90a119
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-09-08 14:45:53 +0300

    Update MainServices.jsx

--- src/components/Services/MainServices.jsx
+++ src/components/Services/MainServices.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Implementation from "../Services/Implementation/Implementation";
-import Training from "../Services/Training/Training";
+import Training from "../Services/Training/Training.jsx";
 import Support from "../Support/Support";
 import Customization from "../Services/Customization/Customization";
 import IntegrationMain from "../Services/Integration/IntegrationMain";
